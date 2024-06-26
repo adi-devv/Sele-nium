1f3b23eadf9043c82fbe85ddbb3c706b6245ba7e
adi-devv <aaditsingal7859@gmail.com>
2024-06-26 16:07:21 +0530

    init 2

--- cookieclicker.py
+++ cookieclicker.py
@@ -16,24 +16,6 @@ eng.click()
 time.sleep(5)
 cookie = driver.find_element(By.ID, "bigCookie")
 
-## TIME LIMIT
-
-# def upgrade():
-#     while True:
-#         time.sleep(5)
-#
-#         buys = driver.find_elements(By.CSS_SELECTOR, ".crate.upgrade.enabled, .product.unlocked.enabled")
-#         print(len(buys))
-#
-#         for i in buys:
-#             i.click()
-#             print("Clicked")
-#
-#
-# thread_upgrades = threading.Thread(target=upgrade)0l
-# thread_upgrades.daemon = True
-# thread_upgrades.start()
-
 while True:
     cookie.click()
     buys = driver.find_elements(By.CSS_SELECTOR, ".crate.upgrade.enabled, .product.unlocked.enabled")
