96d8bed02d20700ab62a407e71bbf135e663a765
adi-devv <aaditsingal7859@gmail.com>
2024-07-01 14:23:27 +0530

    init 2

---
+++ InstaBot.py
@@ -0,0 +1,29 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+options.add_argument(r"user-data-dir=C:\Users\aadit\AppData\Local\Google\Chrome\User Data")  # Update this path
+
+driver = webdriver.Chrome(options=options)
+driver.get(
+    "https://www.instagram.com/johnderting/followers/"
+)
+
+time.sleep(2)
+
+count = driver.find_element(By.XPATH, "//a[@href='/johnderting/followers/']")
+count.click()
+
+time.sleep(2)
+
+flws = driver.find_elements(By.CSS_SELECTOR, "._acan._acap._acas._aj1-._ap30")
+for i in flws:
+    try:
+        i.click()
+    except Exception as e:
+        print(e)
+        continue
