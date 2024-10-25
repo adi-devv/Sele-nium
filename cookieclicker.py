468724e357029b9052dfde30d43bf5f03cb84be2
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-10-25 22:36:51 +0530

    Update cookieclicker.py

--- cookieclicker.py
+++ cookieclicker.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://orteil.dashnet.org/cookieclicker/')
 time.sleep(5)
 
-eng = driver.find_element(By.CSS_SELECTOR, '#langSelect-EN')
+eng = driver.find_element(By.CSS_SELECTOR,  '#langSelect-EN')
 eng.click()
 time.sleep(5)
 cookie = driver.find_element(By.ID, 'bigCookie')
